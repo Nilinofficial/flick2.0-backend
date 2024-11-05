34df11455b3277d32f2f72f0a06352246f1f1a3d
Nilin <itsmenilin18@gmail.com>
2024-11-05 21:44:11 +0530

    add routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 import express from "express";
 import "dotenv/config";
 
+// routes
+import authRouter from "./routes/authRoutes";
+import userRouter from "./routes/userRoutes";
+import matchRouter from "./routes/matchRoutes";
+import messageRouter from "./routes/messageRoutes";
+
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/matches", matchRouter);
+app.use("/api/messages", messageRouter);
 
 app.listen(PORT, () => {
   console.log(`listening to PORT ${PORT}`);
