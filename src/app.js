bb24f2c721b6beb88fd2aa1c1d65a251989cb8ae
Nilin <itsmenilin18@gmail.com>
2024-11-07 21:17:22 +0530

    Add user model

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ import express from "express";
 import "dotenv/config";
 
 // routes
-import authRouter from "./routes/authRoutes";
-import userRouter from "./routes/userRoutes";
-import matchRouter from "./routes/matchRoutes";
-import messageRouter from "./routes/messageRoutes";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import matchRouter from "./routes/matchRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import { connectToDB } from "./config/db.js";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -17,6 +18,6 @@ app.use("/api/users", userRouter);
 app.use("/api/matches", matchRouter);
 app.use("/api/messages", messageRouter);
 
-app.listen(PORT, () => {
-  console.log(`listening to PORT ${PORT}`);
-});
+connectToDB().then(() =>
+  app.listen(PORT, () => console.log(`listening to PORT ${PORT}`))
+);
